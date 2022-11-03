dee5fc8556cd4744d963c0ee66394116282ef331
amer-palo <amer.190501@hotmail.com>
2022-11-04 03:00:01 +1100

    More UI Stuff

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Start");
+    }
+
+    public void Quitting()
+    {
+        Debug.Log("QUITTING!");
+        Application.Quit();
+    }
+}
