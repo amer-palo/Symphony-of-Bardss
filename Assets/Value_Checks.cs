d055f227bf1d57c1b53f08eee6dcb562a13ac463
TheRageofGod <spacedwarf1@gmail.com>
2022-11-28 01:46:40 +1100

    Added fonts and value check script

---
+++ Assets/Value_Checks.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Value_Checks : MonoBehaviour
+{
+   public int Brom1;
+   public int Brom2;
+   public int Aden1;
+   public int Aden2;
+   public int Agatha1;
+   public int Agatha2;
+   public int Jon1;
+   public int Jon2;
+
+   public int Keys;
+    public int helpJon;
+   public int fightMonsters;
+   public int talkToPeople;
+}
